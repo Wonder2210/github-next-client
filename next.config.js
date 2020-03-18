85cc523e3b396c7e2172f12bf429fe5be631c9ab
wonder <gonzalezwonderly@gmail.com>
2020-03-18 15:52:08 -0400

    added keys to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
 
 module.exports = {
     env: {
-      githubClientId: process.env.GITHUB_CLIENT_ID,
-      githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+      githubClientId: Iv1.b838fabcabcb0f23,
+      githubClientSecret: e06002ad6728f9e54a28578a5a11642fc87a5638
   
-      githubClientId: process.env.GITHUB_CLIENT_ID,
     },
   };
